c2af640e251935fdca7811fac8a8611e2ee4e806
numero_quadro <dimabelunin777@niuitmo.ru>
2025-01-22 01:46:30 +0300

    update file not working

--- backend/main.go
+++ backend/main.go
@@ -1,56 +1,240 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
-	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"runtime/debug"
+	"syscall"
+	"time"
 
-	"github.com/nspcc-dev/neo-go/pkg/rpc/client"
+	morphclient "git.frostfs.info/TrueCloudLab/frostfs-node/pkg/morph/client"
+	"git.frostfs.info/TrueCloudLab/frostfs-node/pkg/morph/subscriber"
+	"git.frostfs.info/TrueCloudLab/frostfs-node/pkg/util/logger"
+	cid "git.frostfs.info/TrueCloudLab/frostfs-sdk-go/container/id"
+	"git.frostfs.info/TrueCloudLab/frostfs-sdk-go/object"
+	"git.frostfs.info/TrueCloudLab/frostfs-sdk-go/pool"
+	"git.frostfs.info/TrueCloudLab/frostfs-sdk-go/user"
+	"github.com/nspcc-dev/neo-go/pkg/rpcclient"
+	"github.com/nspcc-dev/neo-go/pkg/rpcclient/actor"
+	"github.com/nspcc-dev/neo-go/pkg/rpcclient/gas"
+	"github.com/nspcc-dev/neo-go/pkg/rpcclient/nep17"
+	"github.com/nspcc-dev/neo-go/pkg/util"
 	"github.com/nspcc-dev/neo-go/pkg/wallet"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
 )
 
-// Replace with your own private key, RPC endpoint, etc.
 const (
-	rpcEndpoint  = "http://127.0.0.1:30333"
-	wif          = "<YOUR_WIF_HERE>"
-	contractHash = "0x1234567890abcdef..." // The deployed contract script hash
+	cfgRPCEndpoint      = "rpc_endpoint"
+	cfgRPCEndpointWS    = "rpc_endpoint_ws"
+	cfgWallet           = "wallet"
+	cfgPassword         = "password"
+	cfgNyanContract     = "nyan_contract"
+	cfgStorageNode      = "storage_node"
+	cfgStorageContainer = "storage_container"
+	cfgListenAddress    = "listen_address"
 )
+
+type Server struct {
+	p        *pool.Pool
+	acc      *wallet.Account
+	act      *actor.Actor
+	gasAct   *nep17.Token
+	nyanHash util.Uint160
+	cnrID    cid.ID
+	log      *zap.Logger
+	rpcCli   *rpcclient.Client
+	sub      subscriber.Subscriber
+}
 
 func main() {
-	// Example usage of the backend to interact with the contract
-	cli, err := client.New(context.Background(), rpcEndpoint, client.Options{})
-	if err != nil {
-		log.Fatalf("Failed to create RPC client: %v", err)
-	}
-	defer cli.Close()
+	ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 
-	// Load a wallet account
-	w, err := wallet.NewWalletFromWIF(wif)
-	if err != nil {
-		log.Fatalf("Failed to load wallet from WIF: %v", err)
-	}
-	acc := w.Accounts[0]
-
-	// You may need to sign transactions with your private key
-	err = acc.Decrypt("") // if WIF is not password protected, pass empty string
-	if err != nil {
-		log.Fatalf("Failed to decrypt account: %v", err)
+	if len(os.Args) != 2 {
+		die(fmt.Errorf("invalid args: %v", os.Args))
 	}
 
-	// Example: call "AddParticipant" to add a user
-	txHash, err := cli.InvokeFunction(contractHash, "addParticipant", []interface{}{"Alice"}, acc, nil)
-	if err != nil {
-		log.Fatalf("InvokeFunction error: %v", err)
-	}
-	fmt.Printf("AddParticipant(Alice) TX: %s\n", txHash)
+	viper.GetViper().SetConfigType("yml")
 
-	// You can do other calls in a similar manner,
-	// e.g. shuffleAndStorePairs, getAllParticipants, etc.
+	f, err := os.Open(os.Args[1])
+	die(err)
+	die(viper.GetViper().ReadConfig(f))
+	die(f.Close())
 
-	// Example: call "shuffleAndStorePairs"
-	txHash, err = cli.InvokeFunction(contractHash, "shuffleAndStorePairs", []interface{}{}, acc, nil)
+	s, err := NewServer(ctx)
+	die(err)
+
+	die(s.Listen(ctx))
+}
+
+func NewServer(ctx context.Context) (*Server, error) {
+	rpcCli, err := rpcclient.New(ctx, viper.GetString(cfgRPCEndpoint), rpcclient.Options{})
 	if err != nil {
-		log.Fatalf("shuffleAndStorePairs error: %v", err)
+		return nil, err
 	}
-	fmt.Printf("shuffleAndStorePairs TX: %s\n", txHash)
+
+	w, err := wallet.NewWalletFromFile(viper.GetString(cfgWallet))
+	if err != nil {
+		return nil, err
+	}
+
+	acc := w.GetAccount(w.GetChangeAddress())
+	if err = acc.Decrypt(viper.GetString(cfgPassword), w.Scrypt); err != nil {
+		return nil, err
+	}
+
+	act, err := actor.NewSimple(rpcCli, acc)
+	if err != nil {
+		return nil, err
+	}
+
+	p, err := createPool(ctx, acc, viper.GetString(cfgStorageNode))
+	if err != nil {
+		return nil, err
+	}
+
+	contractNyanHash, err := util.Uint160DecodeStringLE(viper.GetString(cfgNyanContract))
+	if err != nil {
+		return nil, err
+	}
+
+	var cnrID cid.ID
+	if err = cnrID.DecodeString(viper.GetString(cfgStorageContainer)); err != nil {
+		return nil, err
+	}
+
+	neoClient, err := morphclient.New(ctx, acc.PrivateKey(),
+		morphclient.WithEndpoints(morphclient.Endpoint{Address: viper.GetString(cfgRPCEndpointWS), Priority: 1}),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("new morph client: %w", err)
+	}
+
+	if err = neoClient.EnableNotarySupport(); err != nil {
+		return nil, err
+	}
+
+	params := new(subscriber.Params)
+	params.Client = neoClient
+	l, err := logger.NewLogger(nil)
+	if err != nil {
+		return nil, err
+	}
+	params.Log = l
+	sub, err := subscriber.New(ctx, params)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = sub.SubscribeForNotaryRequests(acc.ScriptHash()); err != nil {
+		return nil, err
+	}
+
+	log, err := zap.NewDevelopment()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Server{
+		p:        p,
+		acc:      acc,
+		act:      act,
+		rpcCli:   rpcCli,
+		nyanHash: contractNyanHash,
+		gasAct:   nep17.New(act, gas.Hash),
+		cnrID:    cnrID,
+		log:      log,
+		sub:      sub,
+	}, nil
+}
+
+func (s *Server) Listen(ctx context.Context) error {
+	// Load the local file
+	filePath := "goofyahhdocument.txt" // Path to the file in the root of your repo
+	fileContent, err := os.ReadFile(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to read file: %w", err)
+	}
+
+	// Upload the file to FrostFS
+	err = s.uploadFileToFrostFS(ctx, fileContent, "goofyahhdocument.txt")
+	if err != nil {
+		return fmt.Errorf("failed to upload file to FrostFS: %w", err)
+	}
+
+	// s.log.Info("File uploaded successfully", zap.String("objectID", objID.String()))
+
+	// Start the HTTP server (if needed)
+	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+		s.log.Info("upload request")
+
+		// Handle file upload via HTTP (if needed)
+	})
+
+	return http.ListenAndServe(viper.GetString(cfgListenAddress), nil)
+}
+
+func (s *Server) uploadFileToFrostFS(ctx context.Context, fileContent []byte, filename string) error {
+	var ownerID user.ID
+	user.IDFromKey(&ownerID, s.acc.PrivateKey().PrivateKey.PublicKey)
+
+	// Create a new object
+	obj := object.New()
+	obj.SetContainerID(s.cnrID)
+	obj.SetOwnerID(ownerID)
+
+	attr := *object.NewAttribute()
+	attr.SetKey("filename")
+	attr.SetValue(filename)
+
+	// Add an attribute for the filename
+	obj.SetAttributes(attr)
+
+	// Prepare the object for upload
+	var prm pool.PrmObjectPut
+	prm.SetPayload(bytes.NewReader(fileContent)) // The file bytes as payload
+	prm.SetHeader(*obj)                          // Set object header (container ID, owner, attributes)
+
+	// Upload the object to FrostFS
+	objID, err := s.p.PutObject(ctx, prm)
+	if err != nil {
+		return fmt.Errorf("put object '%s': %w", err)
+	}
+
+	fmt.Print(objID.ObjectID)
+
+	return nil
+}
+
+func createPool(ctx context.Context, acc *wallet.Account, addr string) (*pool.Pool, error) {
+	var prm pool.InitParameters
+	prm.SetKey(&acc.PrivateKey().PrivateKey)
+	prm.AddNode(pool.NewNodeParam(1, addr, 1))
+	prm.SetNodeDialTimeout(5 * time.Second)
+
+	p, err := pool.NewPool(prm)
+	if err != nil {
+		return nil, fmt.Errorf("new Pool: %w", err)
+	}
+
+	err = p.Dial(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("dial: %w", err)
+	}
+
+	return p, nil
+}
+
+func die(err error) {
+	if err == nil {
+		return
+	}
+
+	debug.PrintStack()
+	_, _ = fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+	os.Exit(1)
 }
--- go.mod
+++ go.mod
@@ -1,45 +1,77 @@
-module secretsanta
+module contract
 
 go 1.23.5
 
-require github.com/nspcc-dev/neo-go/pkg/interop v0.0.0-20250116113717-16a6a59c4342
+require (
+	git.frostfs.info/TrueCloudLab/frostfs-node v0.44.4
+	git.frostfs.info/TrueCloudLab/frostfs-sdk-go v0.0.0-20241206094944-81c423e7094d
+	github.com/nspcc-dev/neo-go v0.106.3
+	github.com/nspcc-dev/neo-go/pkg/interop v0.0.0-20240727093519-1a48f1ce43ec
+	github.com/spf13/viper v1.19.0
+	go.uber.org/zap v1.27.0
+)
+
+// replace github.com/nspcc-dev/neo-go => git.frostfs.info/TrueCloudLab/neoneo-go v0.106.1-0.20241015133823-8aee80dbdc07
 
 require (
+	git.frostfs.info/TrueCloudLab/frostfs-contract v0.21.0-rc.4 // indirect
+	git.frostfs.info/TrueCloudLab/frostfs-crypto v0.6.0 // indirect
+	git.frostfs.info/TrueCloudLab/frostfs-observability v0.0.0-20241112082307-f17779933e88 // indirect
+	git.frostfs.info/TrueCloudLab/hrw v1.2.1 // indirect
+	git.frostfs.info/TrueCloudLab/rfc6979 v0.4.0 // indirect
+	git.frostfs.info/TrueCloudLab/tzhash v1.8.0 // indirect
+	git.frostfs.info/TrueCloudLab/zapjournald v0.0.0-20240124114243-cb2e66427d02 // indirect
+	github.com/VictoriaMetrics/easyproto v0.1.4 // indirect
 	github.com/antlr4-go/antlr/v4 v4.13.1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/consensys/gnark-crypto v0.14.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
-	github.com/golang/snappy v0.0.1 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/holiman/uint256 v1.3.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mr-tron/base58 v1.2.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/nspcc-dev/go-ordered-json v0.0.0-20240830112754-291b000d1f3b // indirect
-	github.com/nspcc-dev/hrw/v2 v2.0.2 // indirect
-	github.com/nspcc-dev/neo-go v0.107.2 // indirect
-	github.com/nspcc-dev/neofs-api-go/v2 v2.14.1-0.20240827150555-5ce597aa14ea // indirect
-	github.com/nspcc-dev/neofs-sdk-go v1.0.0-rc.12.0.20241205083504-335d9fe90f24 // indirect
 	github.com/nspcc-dev/rfc6979 v0.2.3 // indirect
-	github.com/nspcc-dev/tzhash v1.8.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/pierrec/lz4 v2.6.1+incompatible // indirect
 	github.com/prometheus/client_golang v1.20.2 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.55.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
-	github.com/syndtr/goleveldb v1.0.1-0.20210305035536-64b5b1c73954 // indirect
+	github.com/sagikazarmark/locafero v0.6.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/cast v1.6.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/ssgreg/journald v1.0.0 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/syndtr/goleveldb v1.0.1-0.20220721030215-126854af5e6d // indirect
 	github.com/twmb/murmur3 v1.1.8 // indirect
 	go.etcd.io/bbolt v1.3.11 // indirect
+	go.opentelemetry.io/otel v1.28.0 // indirect
+	go.opentelemetry.io/otel/trace v1.28.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.27.0 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
 	golang.org/x/exp v0.0.0-20240823005443-9b4947da3948 // indirect
 	golang.org/x/net v0.28.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240528184218-531527333157 // indirect
-	google.golang.org/grpc v1.65.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240701130421-f6361c86f094 // indirect
+	google.golang.org/grpc v1.66.2 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
